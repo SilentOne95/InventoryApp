bf201dcc7e6268769b3762820536762dff035839
SilentOne <SilentOne@onet.pl>
2018-07-13 00:43:23 +0200

    Working on UI.

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.inventoryapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_insert_dummy_data:
+                return true;
+            case R.id.action_delete_all_entries:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
